b10025601f97005a0e890dc2e10a2aaf01976e66
Alexis Leclerc <alexis@alxs.fr>
2018-11-09 23:48:03 +0200

    dummy classes for CI build

--- Assets/Scripts/TextTimer.cs
+++ Assets/Scripts/TextTimer.cs
@@ -13,7 +13,6 @@ public class TextTimer : MonoBehaviour {
 
     public void Ok()
     {
-
     }
 
     protected void Start()
---
+++ Assets/Tests/TextTimerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TextTimerTest {
+
+        [Test]
+        public void TextTimerTestSimplePasses() {
+            // Use the Assert class to test conditions.
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [UnityTest]
+        public IEnumerator TextTimerTestWithEnumeratorPasses() {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            yield return null;
+        }
+    }
+}
